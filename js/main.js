f5b639b395aa658567d52a1c37434e06583f9b6b
AhmedElabrass123 <ahmedelabrass99@gmail.com>
2023-08-25 04:32:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// var item =document.getElementById("displayOtherLinks");
+// let menu=document.getElementById("menu");
+// item.addEventListener("click",(e)=>{
+//     menu.classList.toggle("active")
+// })
+// document.querySelector("body").addEventListener("click",(e)=>{
+//     menu.style.opacity="0";
+
+// })
+// ================arrow go to up=======>>
+let goUp=document.getElementById("go-up");
+goUp.addEventListener("click",(e)=>{
+            window.scrollTo({
+                top:0,
+                bahavior:"smooth"
+            })    
+})
+
+  
